3f58588fedcc943539eebce355f5a1a7840da460
root <root@ip-172-31-40-254.ap-south-1.compute.internal>
2021-07-23 15:37:28 +0000

    This is 3rd Commit

--- sample.sh
+++ sample.sh
@@ -6,3 +6,4 @@ sg=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
 nw=$(curl -sL http://169.254.169.254/latest/meta-data/network)
 echo "$iid My instance id, $mac MAC no is, $it my instance type is, $sg Security group is, $nw Network is" 
 echo "Hello welcome to git "
+echo "Hello Irlapati Nagababu"
