c7c0fae39f3d152c2ddde91de489fa8fb82287a8
root <root@ip-172-31-40-254.ap-south-1.compute.internal>
2021-07-23 15:29:52 +0000

    This is first commit

---
+++ sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+iid=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
+mac=$(curl -sL http://169.254.169.254/latest/meta-data/mac)
+it=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
+sg=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
+nw=$(curl -sL http://169.254.169.254/latest/meta-data/network)
+echo "$iid My instance id, $mac MAC no is, $it my instance type is, $sg Security group is, $nw Network is" 
+
